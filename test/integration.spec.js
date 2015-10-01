67b5921736f19e425dc1ba4aa11f3dd83518b807
seanstrom <sean@seanhagstrom.com>
2015-09-30 20:47:32 -0700

    feature: custom options to disable selenium startup
    useful for external server integration

--- test/integration.spec.js
+++ test/integration.spec.js
@@ -1,45 +1,71 @@
-var helper = require('./helpers')
-var expect = helper.expect
-var sinon = helper.sinon
+var assign = require('object-assign')
+var seleniumHelper = require('../lib/helpers/selenium')
+var pageVisitingBehavior = require('./shared/integration')
 var server = require('./helpers/server')
 var harness = require('../lib')
-
-var options = {
-  webdriverio: {
-    remote: {
-      desiredCapabilities: { browserName: 'phantomjs' }
-    }
-  }
-}
+var isCI = !!process.env.CI
 
 describe('WebdriverIO Selenium Harness integration', function () {
   before(function (done) {
-    var self = this
-    this.harnessState = harness.setup(options)
-    return this.harnessState.then(function (state) {
-      self.browser = state.browser
-      self.selenium = state.selenium
-      server.listen(9000, done)
-    })
+    var options = this.options = {
+      webdriverio: {
+        remote: { desiredCapabilities: { browserName: 'phantomjs' } }
+      }
+    }
+    server.listen(9000, done)
   })
 
-  after(function () {
-    return this.harnessState
-      .then(harness.teardown)
-      .then(function () { server.close() })
-  })
+  after(function () { server.close() })
 
-  describe('Feature: Test Page', function () {
-    context('When I visit the Test Page', function () {
-      it('contains proper title', function () {
-        var client = this.browser
-        return client
-          .url('http://localhost:9000')
-          .getTitle()
-          .then(function (title) {
-            expect(title).to.equal('Test Page')
-          })
+  describe('With local selenium', function () {
+    before(function () {
+      var self = this
+      var options = self.options = assign({}, this.options, {
+        custom: { remoteSelenium: isCI }
+      })
+      var harnessState = self.harnessState = harness.setup(options)
+      return harnessState.then(function (state) {
+        self.browser = state.browser
+        self.selenium = state.selenium
       })
     })
+
+    after(function () {
+      return this.harnessState.then(harness.teardown)
+    })
+
+    pageVisitingBehavior()
+  })
+
+  describe('With remote selenium', function () {
+    function getSeleniumPromise (isCI) {
+      if (isCI) {
+        return Promise.resolve({ kill: function () {} })
+      }
+      return seleniumHelper.setup({})
+    }
+
+    before(function () {
+      var self = this
+      var options = self.options = assign({}, this.options, {
+        custom: { remoteSelenium: true }
+      })
+
+      return getSeleniumPromise(isCI).then(function (child) {
+        self.selenium = child
+        return harness.setup(options).then(function (state) {
+          self.browser = state.browser
+        })
+      })
+    })
+
+    after(function () {
+      var self = this
+      return self.browser.end().then(function () {
+        return seleniumHelper.teardown(self.selenium)
+      })
+    })
+
+    pageVisitingBehavior()
   })
 })
