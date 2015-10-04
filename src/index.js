76a22c3c1658ae794fb7ea86ee90da98cf4346c9
seanstrom <sean@seanhagstrom.com>
2015-10-03 22:11:47 -0700

    reorganize code

--- src/index.js
+++ src/index.js
@@ -3,29 +3,6 @@ var webdriverio = require('webdriverio')
 var seleniumHelper = require('./helpers/selenium')
 var seleniumStub = { kill: function () {}}
 
-function setup (opts) {
-  opts = formatOptions(opts)
-  var remoteSelenium = opts.custom.remoteSelenium
-
-  if (remoteSelenium) { return build(opts.webdriverio, seleniumStub) }
-
-  return seleniumHelper
-    .setup(opts.selenium)
-    .then(function (child) { return build(opts.webdriverio, child) })
-}
-
-function teardown (state) {
-  return state.browser.end().then(function () {
-    return seleniumHelper.teardown(state.selenium)
-  })
-}
-
-function build(opts, child) {
-  var client = makeClient(opts.remote, opts.init)
-  var state = makeHarnessState(client, child)
-  return validateState(state)
-}
-
 function formatOptions (opts) {
   opts = opts || {}
   opts.custom = opts.custom || {}
@@ -46,13 +23,34 @@ function makeHarnessState (client, child) {
   return { browser: client, selenium: child }
 }
 
-function validateState (state) {
+function build (opts, child) {
+  var client = makeClient(opts.remote, opts.init)
+  var state = makeHarnessState(client, child)
   return state.browser
     .then(function () { return state })
     .catch(function (err) {
       return teardown(state)
         .then(function () { Promise.reject(err) })
     })
+}
+
+function setup (opts) {
+  opts = formatOptions(opts)
+  var remoteSelenium = opts.custom.remoteSelenium
+
+  if (remoteSelenium) {
+    return build(opts.webdriverio, seleniumStub)
+  }
+
+  return seleniumHelper
+    .setup(opts.selenium)
+    .then(function (child) { return build(opts.webdriverio, child) })
+}
+
+function teardown (state) {
+  return state.browser.end().then(function () {
+    return seleniumHelper.teardown(state.selenium)
+  })
 }
 
 exports.setup = setup
