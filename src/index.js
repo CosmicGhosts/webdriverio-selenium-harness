5424188c8c6b93cc5c095789169dc08995ed0b67
seanstrom <sean@seanhagstrom.com>
2015-10-29 18:04:00 -0700

    updates work WIP

--- src/index.js
+++ src/index.js
@@ -4,19 +4,26 @@ var seleniumHelper = require('./helpers/selenium')
 var seleniumStub = { kill: function(){} }
 
 function formatOptions (opts) {
+  var method = 'remote'
+  if (opts.webdriverio && opts.webdriverio.multiremote) {
+    method = 'multiremote'
+  }
+
   opts = opts || {}
   opts.custom = opts.custom || {}
   opts.selenium = opts.selenium || {}
   opts.webdriverio = opts.webdriverio || {}
   opts.webdriverio.remote = opts.webdriverio.remote || {}
+  opts.webdriverio.multiremote = opts.webdriverio.multiremote || {}
   opts.webdriverio.init = opts.webdriverio.init || {}
+  opts.webdriverio.method = method
   return opts
 }
 
-function makeClient (remote, init) {
-  return webdriverio
-    .remote(remote)
-    .init(init)
+function makeClient (opts) {
+  var webdriverioFn = webdriverio[opts.method]
+  var options = opts[opts.method]
+  return webdriverioFn(options).init(init)
 }
 
 function makeHarnessState (client, child) {
@@ -24,7 +31,7 @@ function makeHarnessState (client, child) {
 }
 
 function build (opts, child) {
-  var client = makeClient(opts.remote, opts.init)
+  var client = makeClient(opts)
   var state = makeHarnessState(client, child)
   return state.browser
     .then(function () { return state })
